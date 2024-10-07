ed47d364f577483e645def02084b71303bec831d
mvn23 <schopdiedwaas@gmail.com>
2024-10-07 14:08:30 +0200

    Update setup.py (#8)

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
-import sys
+
 from setuptools import setup
 
-sys.path[0:0] = ['pystove']
-from version import __version__
+from pystove.version import __version__
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
 
 
 setup(
@@ -19,15 +19,14 @@ setup(
     license="GPLv3+",
     keywords="stove hwam smartcontrol",
     url="https://github.com/mvn23/pystove",
-    packages=['pystove'],
-    long_description=read('README.md'),
+    packages=["pystove"],
+    long_description=read("README.md"),
     install_requires=[
-        'aiohttp',
+        "aiohttp",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: GNU General Public License v3 or later "
-        "(GPLv3+)"
+        "License :: OSI Approved :: GNU General Public License v3 or later " "(GPLv3+)",
     ],
 )
