28854056696184325c22ea1d1f19aba908cd80cb
mvn23 <schopdiedwaas@gmail.com>
2025-02-06 01:01:59 +0100

    Convert const lists to Enum types (#26)
    * Convert MAINTENANCE_ALARMS to MaintenanceAlarm IntFlag
    
    * Convert NIGHT_LOWERING_STATES to NightLoweringState IntEnum
    
    * Convert OPERATION_MODES to OperationMode IntEnum
    
    * Convert PHASE to BurnPhase IntEnum
    
    * Convert SAFETY_ALARMS to SafetyAlarm IntFlag
    
    * Convert SELF_TEST_VALUES to SelfTestState IntEnum

--- pystove/const.py
+++ pystove/const.py
@@ -16,6 +16,8 @@
 # Copyright 2019 Milan van Nugteren
 #
 
+from enum import IntEnum, IntFlag
+
 DATA_ALGORITHM = "algorithm"
 DATA_BEGIN_HOUR = "begin_hour"
 DATA_BEGIN_MINUTE = "begin_minute"
@@ -64,71 +66,12 @@ DATA_VALVE1_POSITION = "valve1_position"
 DATA_VALVE2_POSITION = "valve2_position"
 DATA_VALVE3_POSITION = "valve3_position"
 
-MAINTENANCE_ALARMS = [
-    "Stove Backup Battery Low",
-    "O2 Sensor Fault",
-    "O2 Sensor Offset",
-    "Stove Temperature Sensor Fault",
-    "Room Temperature Sensor Fault",
-    "Communication Fault",
-    "Room Temperature Sensor Battery Low",
-]
 
-NIGHT_LOWERING_STATES = [
-    "Disabled",
-    "Init",
-    "Day",
-    "Night",
-    "Manual Night",
-]
-
-OPERATION_MODES = [
-    "Init",
-    "Self Test",
-    "Normal",
-    "Temperature Fault",
-    "O2 Fault",
-    "Calibration",
-    "Safety",
-    "Manual",
-    "MotorTest",
-    "Slow Combustion",
-    "Low Voltage",
-]
-
-PHASE = [
-    "Ignition",
-    "Burn",
-    "Burn",
-    "Burn",
-    "Glow",
-    "Standby",
-]
-
-SAFETY_ALARMS = [
-    "Valve Fault",
-    "Valve Fault",
-    "Valve Fault",
-    "Bad Configuration",
-    "Valve Disconnected",
-    "Valve Disconnected",
-    "Valve Disconnected",
-    "Valve Calibration Error",
-    "Valve Calibration Error",
-    "Valve Calibration Error",
-    "Chimney Overheat",
-    "Door Open Too Long",
-    "Manual Safety Alarm",
-    "Stove Sensor Fault",
-]
-
-SELF_TEST_VALUES = [
-    "Failed",
-    "Passed",
-    "Running",
-    "Not Completed",
-    "Not Started",
-]
+class BurnPhase(IntEnum):
+    IGNITION = 0
+    BURN = 1
+    GLOW = 4
+    STANDBY = 5
 
 
 class FileOpenFailedError(Exception):
@@ -137,3 +80,70 @@ class FileOpenFailedError(Exception):
 
 class FileWriteFailedError(Exception):
     """File Write request unsuccessful."""
+
+
+class MaintenanceAlarm(IntFlag):
+    """Maintenance alarms."""
+
+    BACKUP_BATTERY_LOW = 1
+    O2_SENSOR_FAULT = 2
+    O2_SENSOR_OFFSET = 4
+    STOVE_TEMP_SENSOR_FAULT = 8
+    ROOM_TEMP_SENSOR_FAULT = 16
+    COMM_FAULT = 32
+    ROOM_TEMP_SENSOR_BATTERY_LOW = 64
+
+
+class NightLoweringState(IntEnum):
+    """Night lowering states."""
+
+    DISABLED = 0
+    INIT = 1
+    DAY = 2
+    NIGHT = 3
+    MANUAL_NIGHT = 4
+
+
+class OperationMode(IntEnum):
+    """Stove operation modes."""
+
+    INIT = 0
+    SELF_TEST = 1
+    NORMAL = 2
+    TEMPERATURE_FAULT = 3
+    O2_FAULT = 4
+    CALIBRATION = 5
+    SAFETY = 6
+    MANUAL = 7
+    MOTOR_TEST = 8
+    SLOW_COMBUSTION = 9
+    LOW_VOLTAGE = 10
+
+
+class SafetyAlarm(IntFlag):
+    """Stove Safety Alarms."""
+
+    VALVE_1_FAULT = 1
+    VALVE_2_FAULT = 2
+    VALVE_3_FAULT = 4
+    BAD_CONFIGURATION = 8
+    VALVE_1_DISCONNECTED = 16
+    VALVE_2_DISCONNECTED = 32
+    VALVE_3_DISCONNECTED = 64
+    VALVE_1_CALIBRATION_ERROR = 128
+    VALVE_2_CALIBRATION_ERROR = 256
+    VALVE_3_CALIBRATION_ERROR = 512
+    CHIMNEY_OVERHEAT = 1024
+    DOOR_OPEN_TOO_LONG = 2048
+    MANUAL_SAFETY_ALARM = 4096
+    STOVE_SENSOR_FAULT = 8192
+
+
+class SelfTestState(IntEnum):
+    """Self test states."""
+
+    FAILED = 0
+    PASSED = 1
+    RUNNING = 2
+    NOT_COMPLETED = 3
+    NOT_STARTED = 4
--- pystove/pystove.py
+++ pystove/pystove.py
@@ -115,7 +115,9 @@ class Stove:
         data = await self.get_raw_data()
         if data is None:
             return
-        phase = c.PHASE[data[c.DATA_PHASE]]
+        phase = c.BurnPhase(
+            data[c.DATA_PHASE] if data[c.DATA_PHASE] not in (2, 3) else 1
+        )
         stove_datetime = datetime(
             data[YEAR],
             data[MONTH],
@@ -129,12 +131,8 @@ class Stove:
             minutes=data[c.DATA_NEW_FIREWOOD_MINUTES],
         )
         refuel_estimate = stove_datetime + time_to_refuel
-        maintenance_alarms = self._get_maintenance_alarms_text(
-            data[c.DATA_MAINTENANCE_ALARMS]
-        )
-        safety_alarms = self._get_safety_alarms_text(data[c.DATA_SAFETY_ALARMS])
-        operation_mode = c.OPERATION_MODES[data[c.DATA_OPERATION_MODE]]
-        night_lowering = c.NIGHT_LOWERING_STATES[data[c.DATA_NIGHT_LOWERING]]
+        operation_mode = c.OperationMode(data[c.DATA_OPERATION_MODE])
+        night_lowering = c.NightLoweringState(data[c.DATA_NIGHT_LOWERING])
         # Stove uses 24:00 for end of day
         nighttime_start = time(
             hour=data[c.DATA_NIGHT_BEGIN_HOUR] % 24,
@@ -162,7 +160,9 @@ class Stove:
         processed_data = {
             c.DATA_ALGORITHM: data[c.DATA_ALGORITHM],
             c.DATA_BURN_LEVEL: data[c.DATA_BURN_LEVEL],
-            c.DATA_MAINTENANCE_ALARMS: maintenance_alarms,
+            c.DATA_MAINTENANCE_ALARMS: c.MaintenanceAlarm(
+                data[c.DATA_MAINTENANCE_ALARMS]
+            ),
             c.DATA_MESSAGE_ID: data[c.DATA_MESSAGE_ID],
             c.DATA_NEW_FIREWOOD_ESTIMATE: refuel_estimate,
             c.DATA_NIGHT_BEGIN_TIME: nighttime_start,
@@ -175,7 +175,7 @@ class Stove:
             c.DATA_REMOTE_REFILL_ALARM: data[c.DATA_REMOTE_REFILL_ALARM],
             c.DATA_REMOTE_VERSION: remote_version,
             c.DATA_ROOM_TEMPERATURE: data[c.DATA_ROOM_TEMPERATURE],
-            c.DATA_SAFETY_ALARMS: safety_alarms,
+            c.DATA_SAFETY_ALARMS: c.SafetyAlarm(data[c.DATA_SAFETY_ALARMS]),
             c.DATA_STOVE_TEMPERATURE: data[c.DATA_STOVE_TEMPERATURE],
             c.DATA_TIME_SINCE_REMOTE_MSG: data[c.DATA_TIME_SINCE_REMOTE_MSG],
             c.DATA_DATE_TIME: stove_datetime,
@@ -421,24 +421,6 @@ class Stove:
         )
         return result.get(KEY_RESPONSE) == RESPONSE_OK
 
-    def _get_maintenance_alarms_text(self, bitmask):
-        """Process maintenance alarms bitmask, return a list of strings."""
-        num_alarms = len(c.MAINTENANCE_ALARMS)
-        ret = []
-        for i in range(num_alarms):
-            if 1 << i & bitmask:
-                ret.append(c.MAINTENANCE_ALARMS[i])
-        return ret
-
-    def _get_safety_alarms_text(self, bitmask):
-        """Process safety alarms bitmask, return a list of strings."""
-        num_alarms = len(c.SAFETY_ALARMS)
-        ret = []
-        for i in range(num_alarms):
-            if 1 << i & bitmask:
-                ret.append(c.SAFETY_ALARMS[i])
-        return ret
-
     async def _get_json(self, url):
         """Get data from url, interpret as json, return result."""
         json_str = await self._get(url)
@@ -495,7 +477,7 @@ class _SelfTest:
                     return in_dict
                 out_dict = {}
                 for k, v in in_dict.items():
-                    out_dict[k] = c.SELF_TEST_VALUES[v]
+                    out_dict[k] = c.SelfTestState(v)
                 return out_dict
 
             intermediate_raw = await self.stove._self_test_result()
@@ -503,7 +485,7 @@ class _SelfTest:
                 return None
             else:
                 intermediate = process_dict(intermediate_raw)
-                if 2 not in intermediate_raw.values():
+                if c.SelfTestState.RUNNING not in intermediate.values():
                     self.test_finished = True
                 return intermediate
 
